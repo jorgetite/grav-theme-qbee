506caf564a4ad60e5a6b192af87c0c2c099132fb
Jorge Tite <jorgetite@me.com>
2018-06-06 20:27:28 -0400

    Splash page initial implementation

---
+++ js/theme.js
@@ -0,0 +1,15 @@
+/*******************************************************************
+  QBoss. Grav-theme
+    
+    Copyright (c) 2018, Jorge Tite.
+	  License: MIT
+*******************************************************************/
+
+(function($) {
+    jQuery(document).ready(function() {
+
+        let win = $(window);
+
+        //win.stellar();
+    });
+})(jQuery);
